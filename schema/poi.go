04ef6bee3d5138b110e15ca125b24dd462205fe7
Rachael <rachael@bitmak.com>
2020-06-11 12:23:33 +0800

    add poi_rating schema

--- schema/poi.go
+++ schema/poi.go
@@ -11,24 +11,26 @@ const (
 )
 
 type POI struct {
-	ID        primitive.ObjectID `bson:"_id"`
-	Location  *GeoJSON           `bson:"location"`
-	Score     float64            `bson:"score"`
-	Metric    Metric             `bson:"metric"`
-	Country   string             `bson:"country" json:"-"`
-	State     string             `bson:"state" json:"-"`
-	County    string             `bson:"county" json:"-"`
-	PlaceType string             `bson:"place_type" json:"-"`
+	ID              primitive.ObjectID `bson:"_id"`
+	Location        *GeoJSON           `bson:"location"`
+	Score           float64            `bson:"score"`
+	Metric          Metric             `bson:"metric"`
+	Country         string             `bson:"country" json:"-"`
+	State           string             `bson:"state" json:"-"`
+	County          string             `bson:"county" json:"-"`
+	PlaceType       string             `bson:"place_type" json:"-"`
+	ResourceRatings POIRatingsMetric   `bson:"resource_ratings" json:"-"`
 }
 
 type ProfilePOI struct {
-	ID        primitive.ObjectID `bson:"id" json:"id"`
-	Alias     string             `bson:"alias" json:"alias"`
-	Address   string             `bson:"address" json:"address"`
-	Score     float64            `bson:"score" json:"score"`
-	PlaceType string             `bson:"place_type" json:"place_type"`
-	Metric    Metric             `bson:"metric" json:"-"`
-	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+	ID              primitive.ObjectID   `bson:"id" json:"id"`
+	Alias           string               `bson:"alias" json:"alias"`
+	Address         string               `bson:"address" json:"address"`
+	Score           float64              `bson:"score" json:"score"`
+	PlaceType       string               `bson:"place_type" json:"place_type"`
+	Metric          Metric               `bson:"metric" json:"-"`
+	ResourceRatings ProfileRatingsMetric `bson:"resource_ratings" json:"resource_ratings"`
+	UpdatedAt       time.Time            `bson:"updated_at" json:"updated_at"`
 }
 
 // POIDetail is a client response **ONLY** structure since the data come
---
+++ schema/poi_rating.go
@@ -0,0 +1,46 @@
+package schema
+
+var DefaultResources = map[string]string{
+	"resource_1": "plant-based food",
+	"resource_2": "organic food",
+	"resource_3": "low glycemic index (low-GI) food",
+	"resource_4": "farm-to-table food",
+}
+
+type RatingResourceSort []RatingResource
+type POIResourceRatingSort []POIResourceRating
+
+type Resource struct {
+	ID   string `json:"id" bson:"id"`
+	Name string `json:"name" bson:"name"`
+}
+
+type RatingResource struct {
+	Resource `json:"resource" bson:"resource"`
+	Score    float64 `json:"score " bson:"score"`
+}
+
+type ProfileRatingsMetric struct {
+	Resources  []RatingResource `json:"resources" bson:"resources"`
+	LastUpdate int64            `json:"-" bson:"last_update"`
+}
+
+type POIResourceRating struct {
+	Resource   `json:"resource" bson:"resource"`
+	SumOfScore float64 `json:"-" bson:"sum"`
+	Score      float64 `json:"score" bson:"score"`
+	Ratings    int64   `json:"ratings" json:"ratings"`
+}
+
+type POIRatingsMetric struct {
+	Resources  []POIResourceRating `json:"resources" bson:"resources"`
+	LastUpdate int64               `json:"last_update" bson:"last_update"`
+}
+
+func (r RatingResourceSort) Len() int           { return len(r) }
+func (r RatingResourceSort) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+func (r RatingResourceSort) Less(i, j int) bool { return r[i].Score < r[j].Score }
+
+func (r POIResourceRatingSort) Len() int           { return len(r) }
+func (r POIResourceRatingSort) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+func (r POIResourceRatingSort) Less(i, j int) bool { return r[i].Score < r[j].Score }
