3a8339d8c6b5500e7b7b7d8bf16f2fc97499e15d
Rachael <rachael@bitmak.com>
2020-06-11 11:19:46 +0800

    add api resource-ratings

--- store/poi.go
+++ store/poi.go
@@ -131,6 +131,8 @@ type POI interface {
 
 	AddPOIResources(poiID primitive.ObjectID, resources []schema.Resource, lang string) ([]schema.Resource, error)
 	GetPOIResources(poiID primitive.ObjectID, importantOnly bool, lang string) ([]schema.Resource, error)
+	GetPOIResourceMetric(poiID primitive.ObjectID) (schema.POIRatingsMetric, error)
+	UpdatePOIRatingMetric(poiID primitive.ObjectID, ratings []schema.RatingResource) error
 }
 
 // AddPOI inserts a new POI record if it doesn't exist and append it to user's profile
