3527e8a9ff40ca9f6177c48d5517e7bc199b2f62
Rachael <rachael@bitmak.com>
2020-06-14 22:19:22 +0800

    remove mock data

--- schema/poi_rating.go
+++ schema/poi_rating.go
@@ -1,21 +1,5 @@
 package schema
 
-var DefaultResources = map[string]string{
-	"resource_1":  "plant-based food",
-	"resource_2":  "organic food",
-	"resource_3":  "low glycemic index (low-GI) food",
-	"resource_4":  "farm-to-table food",
-	"resource_5":  "Wearing gloves",
-	"resource_6":  "Social distancing measures",
-	"resource_7":  "Good air circulation",
-	"resource_8":  " Whole (unprocessed) foods diet",
-	"resource_9":  "Vegan/plant-based diet",
-	"resource_10": "Vegetarian diet",
-	"resource_11": "Zone diet",
-	"resource_12": "TLC diet",
-	"resource_13": "No-trans fat diet",
-}
-
 type Resource struct {
 	ID        string `json:"id" bson:"id"`
 	Name      string `json:"name" bson:"name"`
