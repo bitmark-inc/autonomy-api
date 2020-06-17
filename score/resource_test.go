4195cf7bcf1cf802733f356532dfdf1624c6e79f
Jim Yeh <jim@bitmark.com>
2020-06-17 16:47:30 +0800

    Fix incorrect import of test module

--- score/resource_test.go
+++ score/resource_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	"github.com/bitmark-inc/autonomy-api/schema"
-	"gopkg.in/go-playground/assert.v1"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestResourceScoreFirstRating(t *testing.T) {
